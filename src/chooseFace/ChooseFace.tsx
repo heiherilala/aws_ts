1485711b8fffa26e325680965a6b23e4c9c6f876
herilala <nyainaherilala@gmail.com>
2022-08-19 02:10:44 +0300

    fix bouton

--- src/chooseFace/ChooseFace.tsx
+++ src/chooseFace/ChooseFace.tsx
@@ -21,30 +21,12 @@ const ChooseFace = (data:importedObject[],faceActive:number | null,setFaceActive
 
     }else{
         return(
-            <>
-                <h2>Choose a face:</h2>
-                {
-                    data.map((donne, key)=>{
-                        if (key==faceActive) {
-                            return(
-                                <button type='button' className="chooseFace btn btn-success">
-                                    {"face "} 
-                                    <span className="badge badge-light">{key}</span>
-                                    <span className="sr-only">unread messages</span>
-                                </button>
-                            )
-                        }else{
-                            return(
-                                <button type='button' className="chooseFace btn btn-secondary" onClick={()=>setFaceActive(key)}>
-                                    {"face "} 
-                                    <span className="badge badge-light">{key}</span>
-                                    <span className="sr-only">unread messages</span>
-                                </button>
-                            )
-                        }
-                    })
-                }
-            </>
+            <div>
+                <h2>{"Detail of the face N°"+faceActive}</h2>
+                <button type='button' className="chooseFace btn btn-success" onClick={()=>setFaceActive(null)}>
+                    {" Return to face selection "} 
+                </button>
+            </div>
         )
 
     }
